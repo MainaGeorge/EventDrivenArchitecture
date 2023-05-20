14efabe79b9e325c7d05172c43176ca423fd4aa8
George <nuriszka@gmail.com>
2023-05-20 09:32:00 +0200

    initial commit

---
+++ Consumer/EventDrivenArchitecture.Consumer/Data/CustomerDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EventDrivenArchitecture.Consumer.Data
+{
+    public class CustomerDbContext : DbContext
+    {
+        public CustomerDbContext()
+        {
+        }
+
+        public CustomerDbContext(DbContextOptions<CustomerDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Consumer/EventDrivenArchitecture.Consumer/Data/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace EventDrivenArchitecture.Consumer.Data
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public Guid ProductId { get; set; }
+        public int Quality { get; set; }
+        public int Quantity { get; set; }
+    }
+}
